a0db343a787ca837e26e45bbaeccc149fc56068c
George Matos <georgemattos@gmail.com>
2021-04-30 09:13:14 -0300

    style: iproved footer presentation

--- src/App.js
+++ src/App.js
@@ -14,20 +14,15 @@ function App() {
           <Routes />
         </main>
         <footer className="footer sticky-footer">
-          <div className="p-4 pb-0">
-            <section className="mb-4">
-              <Icon link="https://github.com/georgematos" class="fab fa-github" />
-              <Icon link="https://www.linkedin.com/in/george-matos-373a8919a/" class="fab fa-linkedin" />
-              <Icon link="https://www.instagram.com/georgematos.o/" class="fab fa-instagram" />
-              <Icon link="https://www.facebook.com/faceDoGeorge/" class="fab fa-facebook" />
-              <Icon link="https://twitter.com/georgemattos" class="fab fa-twitter" />
-              <Icon link="#!" class="fab fa-google" />
-              <div>
-                <p>Movie Search API is a small application that was made for a job interview at Instituto Atlântico</p>
-                <p>© 2020 Copyright</p>
-              </div>
-            </section>
-          </div>
+          <Icon link="https://github.com/georgematos" class="fab fa-github" />
+          <Icon link="https://www.linkedin.com/in/george-matos-373a8919a/" class="fab fa-linkedin" />
+          <Icon link="https://www.instagram.com/georgematos.o/" class="fab fa-instagram" />
+          <Icon link="https://www.facebook.com/faceDoGeorge/" class="fab fa-facebook" />
+          <Icon link="https://twitter.com/georgemattos" class="fab fa-twitter" />
+          <Icon link="#!" class="fab fa-google" />
+
+          <div>Movie Search API is a small application that was made for a job interview at Instituto Atlântico</div>
+          <div>© 2020 Copyright</div>
         </footer>
       </div>
     </MainAppProvider>
