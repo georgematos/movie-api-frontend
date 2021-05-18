a64d28a9905fe66c653f89ed82ee1f706d779cad
George Matos <georgemattos@gmail.com>
2021-05-18 18:34:18 -0300

    test: app component firsts tests

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function App() {
   return (
     <MainAppProvider>
       <div className="container-fluid">
-        <header className="header">
+        <header data-testid="id-title" className="header">
           <h1>Movie Search API</h1>
         </header>
         <main className="content">
           <Routes />
         </main>
-        <footer className="footer sticky-footer">
-          <Icon link="https://github.com/georgematos" class="fab fa-github" />
-          <Icon link="https://www.linkedin.com/in/george-matos-373a8919a/" class="fab fa-linkedin" />
+        <footer data-testid="id-footer" className="footer sticky-footer">
+          <Icon data-testid="id-github-link" link="https://github.com/georgematos" class="fab fa-github" />
+          <Icon data-testid="id-linkedin-link" link="https://www.linkedin.com/in/george-matos-373a8919a/" class="fab fa-linkedin" />
 
           <div>Movie Search API is a small application that was made for a job interview at Instituto Atlântico</div>
-          <div>© 2020 Copyright</div>
+          <div>© 2021 Copyright</div>
         </footer>
       </div>
     </MainAppProvider>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import Icon from './components/Icon/Icon'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App component", () => {
+  test('renders title page', () => {
+    render(<App />)
+    const titleElement = screen.getByTestId("id-title")
+    expect(titleElement).toHaveTextContent(/movie search api/i)
+  });
+
+  test('renders footer component', () => {
+    render(<App />);
+    const footer = screen.getByTestId("id-footer")
+    expect(footer).toBeInTheDocument
+    expect(footer).toHaveClass("footer", "sticky-footer")
+    expect(footer).toHaveTextContent(/Movie Search API is a small application that was made for a job interview at Instituto Atlântico/i)
+  });
+})
+
